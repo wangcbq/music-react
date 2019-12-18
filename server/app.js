cf9b44004071cf37318c242452ad0fb270ec1d2a
wangcb <wangcb@pukkasoft.cn>
2019-12-18 10:59:43 +0800

    📦 添加api接口server

---
+++ server/app.js
@@ -0,0 +1,117 @@
+const fs = require('fs')
+const path = require('path')
+const express = require('express')
+const bodyParser = require('body-parser')
+const request = require('./util/request')
+const packageJSON = require('./package.json')
+const exec = require('child_process').exec
+const cache = require('apicache').middleware
+const os = require('os')
+const CryptoJS = require('crypto-js');
+
+const key = CryptoJS.enc.Utf8.parse('1234123412ABCDEF');//十六位十六进制数作为密钥
+const iv = CryptoJS.enc.Utf8.parse('ABCDEF1234123412');//十六位十六进制数作为密钥偏移量
+
+//解密方法
+function Decrypt(word) {
+  let encryptedHexStr = CryptoJS.enc.Hex.parse(word);
+  let srcs = CryptoJS.enc.Base64.stringify(encryptedHexStr);
+  let decrypt = CryptoJS.AES.decrypt(srcs, key, { iv: iv, mode: CryptoJS.mode.CBC, padding: CryptoJS.pad.Pkcs7 });
+  let decryptedStr = decrypt.toString(CryptoJS.enc.Utf8);
+  return decryptedStr.toString();
+}
+
+// version check
+exec('npm info NeteaseCloudMusicApi version', (err, stdout, stderr) => {
+  if (!err) {
+    let version = stdout.trim()
+    if (packageJSON.version < version) {
+      console.log(`最新版本: ${version}, 当前版本: ${packageJSON.version}, 请及时更新`)
+    }
+  }
+})
+
+const app = express()
+
+// CORS & Preflight request
+app.use((req, res, next) => {
+  if (req.path !== '/' && !req.path.includes('.')) {
+    res.set({
+      'Access-Control-Allow-Credentials': true,
+      'Access-Control-Allow-Origin': req.headers.origin || '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With,Content-Type',
+      'Access-Control-Allow-Methods': 'PUT,POST,GET,DELETE,OPTIONS',
+      'Content-Type': 'application/json; charset=utf-8'
+    })
+  }
+  req.method === 'OPTIONS' ? res.status(204).end() : next()
+})
+
+// cookie parser
+app.use((req, res, next) => {
+  req.cookies = {}, (req.headers.cookie || '').split(/\s*;\s*/).forEach(pair => {
+    let crack = pair.indexOf('=')
+    if (crack < 1 || crack == pair.length - 1) return
+    req.cookies[decodeURIComponent(pair.slice(0, crack)).trim()] = decodeURIComponent(pair.slice(crack + 1)).trim()
+  })
+  next()
+})
+
+// body parser
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// cache
+app.use(cache('2 minutes', ((req, res) => res.statusCode === 200)))
+
+// static
+app.use(express.static(path.join(__dirname, 'public')))
+
+// router
+const special = {
+  'daily_signin.js': '/daily_signin',
+  'fm_trash.js': '/fm_trash',
+  'personal_fm.js': '/personal_fm'
+}
+
+fs.readdirSync(path.join(__dirname, 'module')).reverse().forEach(file => {
+  if (!file.endsWith('.js')) return
+  let route = (file in special) ? special[file] : '/' + file.replace(/\.js$/i, '').replace(/_/g, '/')
+  let question = require(path.join(__dirname, 'module', file))
+
+  app.use(route, (req, res) => {
+    let query = Object.assign({}, req.query, req.body, { cookie: req.cookies })
+    if ('phone' in query) query.phone = Decrypt(query.phone);
+    if ('email' in query) query.email = Decrypt(query.email);
+    if ('password' in query) query.password = Decrypt(query.password);
+    question(query, request)
+      .then(answer => {
+        console.log('[OK]', decodeURIComponent(req.originalUrl))
+        res.append('Set-Cookie', answer.cookie)
+        res.status(answer.status).send(answer.body)
+      })
+      .catch(answer => {
+        console.log('[ERR]', decodeURIComponent(req.originalUrl))
+        if (answer.body.code == '301') answer.body.msg = '需要登录'
+        res.append('Set-Cookie', answer.cookie)
+        res.status(answer.status).send(answer.body)
+      })
+  })
+})
+
+let localhost = ''
+try {
+  const network = os.networkInterfaces()
+  localhost = network[Object.keys(network)[0]][1].address
+} catch (e) {
+  localhost = 'localhost'
+}
+
+const port = process.env.PORT || 3000
+const host = process.env.HOST || localhost
+
+app.server = app.listen(port, host, () => {
+  console.log(`server running @ http://${host ? host : 'localhost'}:${port}`)
+})
+
+module.exports = app
